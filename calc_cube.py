65f813484db43d2be058f1c4e7fa6dd9ef3db0e0
David Abrahams <davidabrahams@sbcglobal.net>
2017-05-05 18:33:15 -0400

    comments

--- calc_cube.py
+++ calc_cube.py
@@ -1,11 +1,15 @@
 
 if __name__ == "__main__":
+    # density of snow compared to water
     snow_density = 0.9
+    # how many feet is cube
     cube_dimension = 6.5
+    # the number of feet on each side of cube
     east = 1.25
     west = 1.25
     north = 1.25
     south = 1.25
+    # the number of feet on top of cube
     top = 1.25
     snow_volume = (cube_dimension**3) - (cube_dimension-east-west) * \
         (cube_dimension-north-south)*(cube_dimension-top)
